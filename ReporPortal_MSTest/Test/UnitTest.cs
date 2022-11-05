1581a0789095c037d9d7660260507faaa3eeef6f
John Edwin Franco <frindex@gmail.com>
2022-11-05 18:28:19 -0500

    updating unit testing retry 1

--- ReporPortal_MSTest/Test/UnitTest.cs
+++ ReporPortal_MSTest/Test/UnitTest.cs
@@ -1,29 +1,27 @@
-﻿using ReportPortal_POM.Models;
-using FluentAssertions;
+﻿using FluentAssertions;
 using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReportPortal_POM.Models;
 using ReportPortal_POM.Util;
 using Test4Net.Logging.Interfaces;
 using Test4Net.Logging.Provider;
 using Test4Net.UI.POM.Page.Interfaces;
 
-namespace ReportPortal_xUnitTest.UnitTest;
+namespace ReportPortal_MSTest.Test;
 
-public class UnitTest
+[TestClass]
+public class UnitTest : BaseTest
 {
-    private readonly IPageFactory PageFactory;
-    private readonly IConfiguration Configuration;
-    private readonly ILogProvider LogProvider = new LogProvider();
-
-    [Fact]
-    [Trait("Category", "Unit")]
+    [TestMethod]
+    [TestCategory("Unit")]
     public void TestGetUrlExtension_ok()
     {
         Configuration.GetUrl("DashboardPath").Should().
             BeEquivalentTo(new Uri(Path.Combine(Configuration["BaseUrl"], Configuration["DashboardPath"])));
     }
 
-    [Fact]
-    [Trait("Category", "Unit")]
+    [TestMethod]
+    [TestCategory("Unit")]
     public void TestGetModelExtension_ok()
     {
         var fakeModel = new AuthModel(PageFactory, Configuration, LogProvider);
